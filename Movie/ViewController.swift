0d54dc88ae228bdd0cc72b245389331a5a4fad0c
Alice <alicerose19th@gmail.com>
2020-05-24 13:24:04 +0700

    init project

---
+++ Movie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie
+//
+//  Created by Ma Alice on 5/24/20.
+//  Copyright © 2020 Ma Alice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
