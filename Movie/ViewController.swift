f6ad57bda9061d40cfa4440b6dc394a9c52c74de
Alice <alicerose19th@gmail.com>
2020-05-26 18:28:47 +0700

    get now playing movie

--- Movie/ViewController.swift
+++ Movie/ViewController.swift
@@ -6,63 +6,31 @@
 //  Copyright © 2020 Ma Alice. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-    let urlBase = "https://api.themoviedb.org/3/"
-    let api = "api_key=c6daf34c1130b89e1e7821159af5b818"
-    var movies = [MovieData]()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("aaaa")
         fetchNowPlayingMovie()
     }
     
-    
-    private func fetchNowPlayingMovie() {
-        let query = "movie/now_playing?"
-        let request = urlBase + query + api + "&language=en-US&page=1"
-        guard let url =  URL(string: request) else {
-            return
-        }
-
-        print(request)
-        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
-            guard let data = data else { return }
-            print(data)
+    func fetchNowPlayingMovie() {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=c6daf34c1130b89e1e7821159af5b818&language=en-US&page=1")
+        let task =  URLSession.shared.dataTask(with: url!) { data, response, error in
+            guard let data = data else {
+                print("err getting data")
+                return
+            }
+            
             do {
-//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else {
-//                    print("found error in fetching data")
-//                    return
-//                }
-//
-//                let list = json["results"] as! [NSDictionary]
-//
-//                for movie in list {
-////                    print(movie)
-//                    let model = try JSONDecoder().decode(MovieData.self, from: data)
-//                    print(model.title)
-//
-////                    print(movie["title"])
-//                }
-
-                guard let json = try? JSONDecoder().decode(Movies.self, from: data) else {
-                    print("found error in fetching data")
-                    return
+                let movies = try JSONDecoder().decode(MovieFeedResult.self, from: data)
+                for movie in movies.results {
+                    print(movie.title ?? "Empty title")
                 }
-                
-                self.movies = json.results
-                print(self.movies[0].title)
-                
+            } catch let error {
+                print(error)
             }
-            catch let jsonError {
-                print(jsonError.localizedDescription)
-            }
-            catch let parsingError {
-                print("Error", parsingError)
-            }
-            print("found something")
         }
         
         task.resume()
